714998b4c77dc7e06d7d264651e7c5b2d0531892
Duo Tao <taod@carleton.edu>
2016-07-13 21:48:00 -0500

    used the same layout

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var Match = require('../models/Match.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Match.find(function(err, matches) {
-    if (err) return console.error(err);
-    res.render('index', { 'matches': matches });
-  });
+    res.render('index', {
+        title: 'Gucci Official Site - Founded In Florence, Italy in 1921.',
+        description: "Shop the Gucci Garden Collection available exclusively on Gucci.com. Designed by Alessandro Michele and featuring a colorful print of flowers & animals."
+    });
 });
 
 module.exports = router;
--- routes/shopTheLooks.js
+++ routes/shopTheLooks.js
@@ -5,7 +5,11 @@ var Match = require('../models/Match.js');
 router.get('/', function(req, res, next) {
   Match.find(function(err, matches) {
     if (err) return console.error(err);
-    res.render('shop-the-looks', { 'matches': matches });
+        // res.render('shop-the-looks', { 'matches': matches });
+        res.render('shop-the-looks', {
+            title: 'Gucci Garden Collection - Exclusively Online',
+            description: "Shop the Gucci Garden Collection available exclusively on Gucci.com. Designed by Alessandro Michele and featuring a colorful print of flowers & animals."
+        });
   });
 });
 
