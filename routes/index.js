79783e8fb800e760a3cc8dd82e92184dd595258e
Duo Tao <taod@carleton.edu>
2016-08-27 17:35:39 -0500

    resumed bad cleanups

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var Match = require('../models/Match.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {
-        title: 'Gucci Official Site - Founded In Florence, Italy in 1921.',
-        description: "Shop the Gucci Garden Collection available exclusively on Gucci.com. Designed by Alessandro Michele and featuring a colorful print of flowers & animals."
+        title: '哈哈哈',
+        description: "哈哈的网站"
     });
 });
 
